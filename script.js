c467208c51298dd41d85a00fbdcdc6e24e8db592
Senay Gebrat <senaygebrat@gmail.com>
2022-09-28 12:56:34 -0700

    functional game

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var userChoice = "";
 
 //create variables for computer's choice
 var computerChoice = "";
-
+var choiceNumber = "";
 //variables for total stats wins losses ties
 var win = 0;
 var loss = 0;
@@ -19,13 +19,80 @@ function checkUserChoice(){
     checkUserChoice();
   }
 }
-function generateComputerChoice(){
-    
-}
-checkUserChoice()
 
-//check that choice is valid (r, p, s)
 //if valid, computer responds with its choice
+function generateComputerChoice(){
+    var choiceNumber = Math.floor(Math.random()*3);
+    console.log(choiceNumber);
+    if(choiceNumber == 0){
+        computerChoice = "r";
+    }
+    if(choiceNumber == 1){
+        computerChoice = "p";
+    }
+    if(choiceNumber == 2){
+        computerChoice = "s";
+    }
+    console.log(computerChoice);
+}
+
+function determineWinner(){
+    if(userChoice == "r" && computerChoice == "r"){
+        alert("Game was a tie! Computer choice was " + computerChoice);
+        (ties++);
+    }
+    if(userChoice == "r" && computerChoice == "p"){
+        alert("You lose! Computer choice was " + computerChoice);
+        (loss++);
+    }
+    if(userChoice == "r" && computerChoice == "s"){
+        alert("You win! Computer choice was " + computerChoice);
+        (win++);
+    }
+    if(userChoice == "p" && computerChoice == "r"){
+        alert("You win! Computer choice was " + computerChoice);
+        (win++);
+    }
+    if(userChoice == "p" && computerChoice == "p"){
+            alert("Game was a tie! Computer choice was " + computerChoice);
+            (ties++);
+    }
+    if(userChoice == "p" && computerChoice == "s"){
+        alert("You lose! Computer choice was " + computerChoice);
+        (loss++);
+    }
+    if(userChoice == "s" && computerChoice == "r"){
+        alert("You lose! Computer choice was " + computerChoice);
+        (loss++);
+    }
+    if(userChoice == "s" && computerChoice == "p"){
+        alert("You win! Computer choice was " + computerChoice);
+        (win++);
+    }
+    if(userChoice == "s" && computerChoice == "s"){
+        alert("Game was a tie! Computer choice was " + computerChoice);
+        (ties++);
+    }
+
+}
+
+function displayStats(){
+    alert("Wins: " + win + " Losses : " + loss + " Ties: " + ties)
+}
+
+
+function playGame(){
+    checkUserChoice()
+    generateComputerChoice()
+    determineWinner()
+    displayStats()
+    playGame()
+}
+
+playGame()
+
+
+
 //display who won, and store win/loss/tie value
 //offer to play again
 
